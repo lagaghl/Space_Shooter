48c4690e8e435b0ccf6f9dde05800b25ea3c31e3
lagaghl <jhonatan.nieva323@gmail.com>
2024-06-12 15:30:53 -0500

    exit???

--- Space_Shooter.py
+++ Space_Shooter.py
@@ -101,7 +101,6 @@ def main_menu():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     return
